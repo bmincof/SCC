f0b56a073c42e69a981bc64db5151df3cb7e5b1a
bmincof <bminlvcoffee@gmail.com>
2023-06-25 00:20:49 +0900

    feat: 범용적인 카드 컴포넌트 구현

--- scc/src/components/compound/MyCard.jsx
+++ scc/src/components/compound/MyCard.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import MyTitle from "../primitve/MyTitle";
 import MySubTitle from "../primitve/MySubTitle";
 import LongText from "../primitve/LongText";
+import CommonButton from "../primitve/button/CommonButton";
 
 // 프로필과 이미지를 포함하는 요소를 나타내기 위한 컴포넌트
 const MyCard = ({
@@ -15,27 +16,43 @@ const MyCard = ({
 }) => {
   return (
     <Container width={width} height={height}>
-      <img alt="이미지가 들어갈 장소" src={src} />
-      <MyTitle value={title} />
-      <MySubTitle value={subtitle} />
-      <LongText value={desc} />
+      <CardThumbnail alt="이미지가 들어갈 장소" src={src} />
+      <CardBody>
+        <MyTitle value={title} />
+        <MySubTitle value={subtitle} />
+        <LongText value={desc} />
+      </CardBody>
+      <CardButton>
+        <CommonButton value={"바로가기"} className="filled" />
+      </CardButton>
     </Container>
   );
 };
 
 const Container = styled.div`
   display: inline-block;
-  text-align: left;
   border: solid 1px;
 
-  width: 240px;
-  height: 300px;
+  width: 20rem;
+`;
 
-  img {
-    background-color: #bbbbbb;
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
+const CardThumbnail = styled.img`
+  background-color: #d9e7cb;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
+
+const CardBody = styled.div`
+  padding: 0.3rem;
+  text-align: left;
+
+  * {
+    margin-bottom: 0.3rem;
   }
+`;
+
+const CardButton = styled.div`
+  text-align: right;
 `;
 
 export default MyCard;
